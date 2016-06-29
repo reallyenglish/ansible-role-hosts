40865c08b937b8cb3c4535ba7c7a69891cf3eaee
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-06-29 09:35:49 +0900

    remove hosts_additional_loopback
    because it's redandunt. same thing can be done with hosts_map.

--- spec/serverspec/default_spec.rb
+++ spec/serverspec/default_spec.rb
@@ -8,6 +8,5 @@ describe file(hosts_file) do
   its(:content) { should match /192\.168\.100\.1\s+foo/ }
   its(:content) { should match /192\.168\.100\.2\s+bar\s+buz/ }
   its(:content) { should match /127\.0\.0\.1\s+localhost/ }
-  its(:content) { should match /127.0.0.1\s+default-ansible-freebsd-103-amd64/ }
   its(:content) { should match /::1\s+ip6-localhost\sip6-loopback/ }
 end
