ae3ace4445b025ef748a871a31d9a0580bde6b3a
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-18 12:37:11 +0900

    support list of extra host name for 127.0.0.1

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,11 +2,22 @@ require "spec_helper"
 require "serverspec"
 
 hosts_file = "/etc/hosts"
+extra_localhost = ["localhost1", "localhost2", "buz.example.org"]
 
 describe file(hosts_file) do
   it { should be_file }
   its(:content) { should match(/192\.168\.100\.1\s+foo/) }
   its(:content) { should match(/192\.168\.100\.2\s+bar\s+buz/) }
-  its(:content) { should match(/127\.0\.0\.1\s+localhost/) }
+  its(:content) { should match(/127\.0\.0\.1\s+localhost\s+#{Regexp.escape(extra_localhost.join(" "))}$/) }
   its(:content) { should match(/::1\s+ip6-localhost\sip6-loopback/) }
+end
+
+describe command("ping -c 1 localhost") do
+  its(:exit_status) { should eq 0 }
+end
+
+extra_localhost.each do |h|
+  describe command("ping -c 1 #{h}") do
+    its(:exit_status) { should eq 0 }
+  end
 end
