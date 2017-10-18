6b7355036b50cfaaccc931b77f23ff392561cbf1
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-18 18:39:09 +0900

    s/extra_localhost/extra_localhosts/g

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -2,13 +2,13 @@ require "spec_helper"
 require "serverspec"
 
 hosts_file = "/etc/hosts"
-extra_localhost = ["localhost1", "localhost2", "buz.example.org"]
+extra_localhosts = ["localhost1", "localhost2", "buz.example.org"]
 
 describe file(hosts_file) do
   it { should be_file }
   its(:content) { should match(/192\.168\.100\.1\s+foo/) }
   its(:content) { should match(/192\.168\.100\.2\s+bar\s+buz/) }
-  its(:content) { should match(/127\.0\.0\.1\s+#{Regexp.escape(extra_localhost.join(" "))}\s+localhost$/) }
+  its(:content) { should match(/127\.0\.0\.1\s+#{Regexp.escape(extra_localhosts.join(" "))}\s+localhost$/) }
   its(:content) { should match(/::1\s+ip6-localhost\sip6-loopback/) }
 end
 
@@ -16,7 +16,7 @@ describe command("ping -c 1 localhost") do
   its(:exit_status) { should eq 0 }
 end
 
-extra_localhost.each do |h|
+extra_localhosts.each do |h|
   describe command("ping -c 1 #{h}") do
     its(:exit_status) { should eq 0 }
   end
